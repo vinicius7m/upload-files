7d4168a3e507d719e8ac07f96f7756cb9cdb3c2f
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-05-13 09:13:39 -0300

    Altering migration user, adding column uuid in User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->string('uuid');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            
             $table->timestamps();
         });
     }

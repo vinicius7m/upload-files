8caa38c528577f3255d1a87e99ebefdda1562692
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-05-13 11:03:18 -0300

    refatoring DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+        $this->call('UserTableSeeder');
+    }
+}
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $user = new \App\User();
+        $user->name = 'Fabio Vedovelli';
+        $user->email = 'vedovelli@gmail.com';
+        $user->password = bcrypt('123456');
+        $user->save();
+
+        $i = 0;
+        while ($i < 10) {
+            $user = new \App\User();
+            $user->name = 'Fabio Vedovelli';
+            $user->email = $i . 'vedovelli@gmail.com';
+            $user->password = bcrypt('123456');
+            $user->save();
+            $i++;
+        }
     }
 }
